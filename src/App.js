8aacb8497fa91d1e8a86b68206f51e88c5352dc5
Luca Perna <luca.perna@thinkopen.it>
2018-04-11 20:44:50 +0200

    little fixes

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{'Welcome to React'}</h1>
+          <h1 className="App-title">{'Scoreboard'}</h1>
         </header>
 
         <div className="App-intro">
